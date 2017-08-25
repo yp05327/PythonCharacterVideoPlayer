4295a472d7641f6d5e7949a776ca1ef493476e6b
eddie yang <576951401@qq.com>
2017-08-26 00:28:04 +0800

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# PythonCharacterVideoPlayer
-【python]【BadApple】实时计算的字符动画，自带音频播放
+## PythonCharacterVideoPlayer
+
+[![Licence](https://img.shields.io/badge/licence-MIT-blue.svg)](https://github.com/yp05327/PythonCharacterVideoPlayer/LICENSE) [![Python](https://img.shields.io/badge/python-2.7%2C3.+-blue.svg)](https://github.com/yp05327/PythonCharacterVideoPlayer#) 
+
+# 说明
+python编写的实时字符动画播放器，实时解码并转换为字符，同时自带音频播放（需要安装ffmpeg，如果使用音频播放，在播放目录下会生成一个临时文件，正常退出时会自动删除）  
+BadApple.mp4:[点击此处](http://odxw2uear.bkt.clouddn.com/BadApple.mp4)
+
+# 更新记录
+[点击此处](https://github.com/yp05327/PythonCharacterVideoPlayer/blob/master/update.md)
+
+# 安装
+运行环境需要python，目前支持的python版本：
+
+```
+python2.7
+python3.+(未测试)
+```
+
+安装依赖库，使用pip，Linux、Mac非root需要添加sudo
+
+```shell
+pip install -r requirements.txt
+```
+
+# 使用
+运行
+
+```shell
+python main.py --各种参数
+```
+
+参数说明：
+* --video_dir 必填，视频文件所在目录，如果在main.py同一目录下则只需要文件名
+* --ascii_mode 灰度值直接映射可见ascii码，不建议使用，默认为False
+* --audio_mode 是否播放音频，需要ffmpeg支持，安装方法自行查询，默认为False
+* --zifu 可替换显示字符，优先级低于--ascii_mode参数，默认为@
+* --video_scale 视频播放比例，数值越大需要越高的计算能力，默认64
+
+示例(未安装ffmpeg)：
+```shell
+python main.py --video_dir BadApple.mp4
+```
+
+示例(安装了ffmpeg)：
+```shell
+python main.py --video_dir BadApple.mp4 --audio_mode True
+```
+
+
---
+++ main.py
@@ -0,0 +1,131 @@
+# coding:utf-8
+import cv2
+import time
+import os
+import pygame
+import argparse
+from pydub import AudioSegment
+
+# 将灰度值转为字符
+def get_char(gray_number):
+    if FLAGS.ascii_mode:
+        return chr(int(93.0/256*gray_number)) + ' '
+    else:
+        if gray_number > 100:
+            return FLAGS.zifu + ' '
+        else:
+            return '  '
+
+# 将图片转为字符
+def img_to_char(image, size):
+    image = cv2.resize(image, size)
+    text = ''
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            text += get_char(image[i, j])
+        text += '\n'
+
+    return text
+
+# 播放一帧
+def play(text, start_played_time, num, fps):
+    now_time = time.time()
+
+    # 帧率控制
+    if now_time - start_played_time - num*1.0/fps < 0:
+        time.sleep(num*1.0/fps - (now_time - start_played_time))
+
+    os.system('clear')
+    total_time = time.time() - start_played_time
+    print(text + '原视频帧率：%f, 当前帧：%d，播放时长：%f，帧率：%f' % (fps, num, total_time, num*1.0/total_time))
+
+# 获取缩放大小
+def get_size(shape):
+    tmp = shape[1]/FLAGS.video_scale
+    size = (shape[1]/tmp, shape[0]/tmp)
+    return size
+
+def run():
+    video = cv2.VideoCapture(FLAGS.video_dir)
+    start_played_time = 0
+
+    # Find OpenCV version
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+    if int(major_ver) < 3:
+        fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+    else:
+        fps = video.get(cv2.CAP_PROP_FPS)
+
+    num = 0
+    while (video.isOpened()):
+        ret, frame = video.read()
+        size = get_size(frame.shape)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        text = img_to_char(gray, size)
+
+        if start_played_time == 0:
+            start_played_time = time.time()
+
+            pygame.mixer.init()
+            track = pygame.mixer.music.load("Audio_tmp.wav")
+            pygame.mixer.music.play()
+
+        play(text, start_played_time, num, fps)
+
+        num += 1
+
+    video.release()
+
+def main():
+    if os.path.isfile('Audio_tmp.mp3'):
+        os.remove('Audio_tmp.mp3')
+
+    if FLAGS.video_dir == '':
+        print('请输入视频路径')
+    else:
+        if FLAGS.audio_mode:
+            AudioSegment.from_file(FLAGS.video_dir, 'mp4').export('Audio_tmp.wav', format='wav')
+
+        run()
+
+        if os.path.isfile('Audio_tmp.mp3'):
+            os.remove('Audio_tmp.mp3')
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--video_dir',
+        type=str,
+        default='',
+        help='视频路径'
+    )
+    parser.add_argument(
+        '--ascii_mode',
+        type=bool,
+        default=False,
+        help='采用灰度转ascii码模式'
+    )
+    parser.add_argument(
+        '--audio_mode',
+        type=bool,
+        default=False,
+        help='是否播放音频，需要pydub和ffmpeg支持'
+    )
+    parser.add_argument(
+        '--zifu',
+        type=str,
+        default='@',
+        help='可以指定替换字符，默认为@'
+    )
+    parser.add_argument(
+        '--video_scale',
+        type=int,
+        default=64,
+        help='像素缩放比例，默认64，值越大需要更高的计算性能'
+    )
+
+    FLAGS, unparsed = parser.parse_known_args()
+    main()
